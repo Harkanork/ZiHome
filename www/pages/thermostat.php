51902010935721d5e4a790663322e749f8785ecf
Cmoi20 <boquetj@yahoo.com>
2013-09-26 10:56:44 +0200

    modified:   www/pages/thermostat.php

--- www/pages/thermostat.php
+++ www/pages/thermostat.php
@@ -4,6 +4,60 @@ if(isset($_SESSION['auth']))
 include("./pages/conf_zibase.php");
 include("./lib/zibase.php");
 $zibase = new ZiBase($ipzibase);
+if(isset($_POST['VALIDER'])) {
+$cal = new ZbCalendar();
+for ($j = 0; $j < 24; $j++) {
+if($_POST[$j] == 'on'){
+$val = 1;
+} else {
+$val = 0;
+}
+$cal->hour[$j] = $val;
+}
+if($_POST['lundi'] == 'on'){
+$lundi = 1;
+} else {
+$lundi = 0;
+}
+if($_POST['mardi'] == 'on'){
+$mardi = 1;
+} else {
+$mardi = 0;
+}
+if($_POST['mercredi'] == 'on'){
+$mercredi = 1;
+} else {
+$mercredi = 0;
+}
+if($_POST['jeudi'] == 'on'){
+$jeudi = 1;
+} else {
+$jeudi = 0;
+}
+if($_POST['vendredi'] == 'on'){
+$vendredi = 1;
+} else {
+$vendredi = 0;
+}
+if($_POST['samedi'] == 'on'){
+$samedi = 1;
+} else {
+$samedi = 0;
+}
+if($_POST['dimanche'] == 'on'){
+$dimanche = 1;
+} else {
+$dimanche = 0;
+}
+$cal->day['lundi'] = $lundi;
+$cal->day['mardi'] = $mardi;
+$cal->day['mercredi'] = $mercredi;
+$cal->day['jeudi'] = $jeudi;
+$cal->day['vendredi'] = $vendredi;
+$cal->day['samedi'] = $samedi;
+$cal->day['dimanche'] = $dimanche;
+$zibase->setCalendar($_POST['id'],$cal);
+}
 if(isset($_POST['Modifier_vcj'])) {
 $zibase->setVariable($_POST['id'],$_POST['temperature']*10);
 }
@@ -36,10 +90,34 @@ echo "<p align=center>Consigne de nuit : ".(($zibase->getVariable($thermostat[$i
 <INPUT TYPE="SUBMIT" NAME="Modifier_vcn" VALUE="Modifier">
 </p></FORM>
 <?
-echo "<p align=center><a href=./index.php?page=calendrier&cal=".$thermostat[$i]['cal'].">Calendrier</a></p>";
 echo "<p align=center>Hysteresis : ".$thermostat[$i]['h']."</p>";
 echo "<p align=center>Actif : ".(($zibase->getVariable($thermostat[$i]['vs']))/10)."</p>";
-
+for ($k = 0; $k < 3; $k++) {
+$calendrier=$zibase->getCalendar($thermostat[$i]['cal']+$k);
+echo "<center>Calendrier Numero ".($thermostat[$i]['cal']+$k)."</center>";
+?>
+<center><p align="center"><form method="post" action="./index.php?page=thermostat">
+<?
+for ($j = 0; $j < 24; $j++) {
+if($j > 0 && $j <> 13) { echo " | "; }
+if($j == 13) { echo "<br>"; }
+?>
+<? echo $j; ?>h <input type=checkbox name=<? echo $j; if($calendrier->hour[$j] == 1) { ?> Checked<? } ?>>
+<? } ?>
+<BR>
+Lundi <input type=checkbox name=lundi<? if($calendrier->day['lundi'] == 1) { ?> Checked<? } ?>>
+ | Mardi <input type=checkbox name=mardi<? if($calendrier->day['mardi'] == 1) { ?> Checked<? } ?>>
+ | Mercredi <input type=checkbox name=mercredi<? if($calendrier->day['mercredi'] == 1) { ?> Checked<? } ?>>
+ | Jeudi <input type=checkbox name=jeudi<? if($calendrier->day['jeudi'] == 1) { ?> Checked<? } ?>>
+ | Vendredi <input type=checkbox name=vendredi<? if($calendrier->day['vendredi'] == 1) { ?> Checked<? } ?>>
+ | Samedi <input type=checkbox name=samedi<? if($calendrier->day['samedi'] == 1) { ?> Checked<? } ?>>
+ | Dimanche <input type=checkbox name=dimanche<? if($calendrier->day['dimanche'] == 1) { ?> Checked<? } ?>>
+<INPUT TYPE="HIDDEN" NAME="id" VALUE="<? echo $thermostat[$i]['cal']+$k; ?>">
+<br>
+<INPUT TYPE="SUBMIT" NAME="VALIDER" VALUE="VALIDER">
+</form></p></center>
+<?
+}
 $i++;
 }
 }
