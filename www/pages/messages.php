65d30ffdc3d45c1c300e0241e7cab041761f9b51
Cmoi20 <boquetj@yahoo.com>
2013-10-09 11:44:42 +0200

    modified:   www/pages/messages.php

--- www/pages/messages.php
+++ www/pages/messages.php
@@ -35,10 +35,16 @@ while ($data = mysql_fetch_assoc($req))
 {
 ?><option value="<? echo $data['date']; ?>"<? if(isset($_POST['Valider'])){ if($_POST['heure'] == $data['date']){ ?> selected<? }} ?>><? echo $data['date']; ?></option><?
 }
-?></select><input type=submit value=Valider name=Valider></form>
+?></select><input type=submit value=Valider name=Valider></form></p>
+<p align=center><form method=post action="./index.php?page=administration&detail=messages">
+<input type=texte name=texte>
+<input type=submit value=Rechercher name=Rechercher></form></p>
+<p align=center>
 <?
 if(isset($_POST['Valider'])) {
 $query = "SELECT * FROM message_zibase WHERE `date` LIKE '".$_POST['annee']."-".$_POST['mois']."-".$_POST['jour']." ".$_POST['heure']."%' ORDER BY id DESC";
+} else if(isset($_POST['Rechercher'])){
+$query = "SELECT * FROM message_zibase WHERE message LIKE '%".$_POST['texte']."%' ORDER BY id DESC LIMIT 100";
 } else {
 $query = "SELECT * FROM message_zibase ORDER BY id DESC LIMIT 100";
 }
@@ -47,5 +53,6 @@ while ($data = mysql_fetch_assoc($req))
 {
 echo $data['date']." : ".$data['message']."<br>";
 }
+?></p><?
 }
 ?>
