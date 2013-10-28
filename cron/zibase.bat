f9f8e9210e8a3843bcaa86c39b32bfa034944ab9
Cmoi20 <boquetj@yahoo.com>
2013-10-28 14:17:25 +0100

    new file:   zibase.bat

---
+++ cron/zibase.bat
@@ -0,0 +1,7 @@
+@echo off
+c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\actioneurs.php
+c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\conso_electrique.php
+c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\scenario.php
+c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\vent.php
+c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\temeprature.php
+
