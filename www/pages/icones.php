c4867d8a23a0a0c652df7f9c2b8563427bb560f2
Cmoi20 <boquetj@yahoo.com>
2013-10-29 16:12:00 +0100

    modified:   www/pages/icones.php 	modified:   www/pages/plan.php

--- www/pages/icones.php
+++ www/pages/icones.php
@@ -16,7 +16,7 @@ $array = array( 1, 2, 4, 5, 6, 7, 8, 9 );
 echo "<p align=center><form method=\"post\" action=\"./index.php?page=administration&detail=icones\">";
 foreach($array as $icones) {
 ?><input type="radio" name="icones" value="<? echo $icones ?>"<? if($icones1 == $icones) { echo " checked"; } ?>><?
-echo "<img src=./img/icones/".$icones."c_logotype_temperature.png><BR>";
+echo "<img src=./img/icones/StyleIconPreview".$icones.".png><BR>";
 
 }
 echo "<INPUT TYPE=\"SUBMIT\" NAME=\"VALIDER\" VALUE=\"VALIDER\"></form></p>";
