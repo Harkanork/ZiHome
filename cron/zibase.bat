bed4e2b7e4ef96e62cef1fc55b4d7ca27b2af570
Cmoi20 <boquetj@yahoo.com>
2013-11-25 19:02:55 +0100

    modified:   zibase.bat

--- cron/zibase.bat
+++ cron/zibase.bat
@@ -3,6 +3,6 @@ c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\actioneurs.php
 c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\conso_electrique.php
 c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\scenario.php
 c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\vent.php
-c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\temeprature.php
+c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\temperature.php
 c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\message_zibase_auth.php
 
