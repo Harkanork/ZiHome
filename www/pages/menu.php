9e99903e26689006096ca38ca1af4e3527ab59e5
gthiriez <gthiriez@hotmail.fr>
2013-12-09 19:36:05 +0000

    1

--- www/pages/menu.php
+++ www/pages/menu.php
@@ -13,42 +13,42 @@ while ($data = mysql_fetch_assoc($req))
 {
 if($data['libelle'] == "conso_elec" && $data['actif'] == 1) {
 ?>
-<li><A HREF="./index.php?pages=conso_elec"><img src = "./img/icon_elec.png"/><span>Conso-Elec</span></a></li>
+<li><A HREF="./index.php?page=conso_elec"><img src = "./img/icon_elec.png"/><span>Conso-Elec</span></a></li>
 <?
 }
 if($data['libelle'] == "temperature" && $data['actif'] == 1) {
 ?>
-<li><A HREF="./index.php?pages=temperature"><img src = "./img/icon_temp.png"/><span>Temperature</span></a></li>
+<li><A HREF="./index.php?page=temperature"><img src = "./img/icon_temp.png"/><span>Temperature</span></a></li>
 <?
 }
 if($data['libelle'] == "owl" && $data['actif'] == 1) {
 ?>
-<li><A HREF="./index.php?pages=owl"><img src = "./img/icon_elec.png"/><span>OWL</span></a></li>
+<li><A HREF="./index.php?page=owl"><img src = "./img/icon_elec.png"/><span>OWL</span></a></li>
 <?
 }
 if($data['libelle'] == "batterie" && $data['actif'] == 1) {
 ?>
-<li><A HREF="./index.php?pages=batterie"><img src = "./img/icon_pile.png"/><span>Batteries</span></a></li>
+<li><A HREF="./index.php?page=batterie"><img src = "./img/icon_pile.png"/><span>Batteries</span></a></li>
 <?
 }
 if($data['libelle'] == "plan" && $data['actif'] == 1) {
 ?>
-<li><a href = "./index.php?pages=plan"><img src = "./img/icon_home.png"/><span>Plan</span></a></li>
+<li><a href = "./index.php?page=plan"><img src = "./img/icon_home.png"/><span>Plan</span></a></li>
 <?
 }
 if($data['libelle'] == "thermostat" && $data['actif'] == 1 && isset($_SESSION['auth'])) {
 ?>
-<li><a href = "./index.php?pages=thermostat"><img src = "./img/icon_temp.png"/><span>Thermostat</span></a></li>
+<li><a href = "./index.php?page=thermostat"><img src = "./img/icon_temp.png"/><span>Thermostat</span></a></li>
 <?
 }
 if($data['libelle'] == "calendrier" && $data['actif'] == 1 && isset($_SESSION['auth'])) {
 ?>
-<li><a href = "./index.php?pages=calendrier"><img src = "./img/icon_calendrier.png"/><span>Calendrier</span></a></li>
+<li><a href = "./index.php?page=calendrier"><img src = "./img/icon_calendrier.png"/><span>Calendrier</span></a></li>
 <?
 }
 if($data['libelle'] == "vent" && $data['actif'] == 1) {
 ?>
-<li><a href = "./index.php?pages=vent"><img src = "./img/icon_vent.png"/><span>Vent</span></a></li>
+<li><a href = "./index.php?page=vent"><img src = "./img/icon_vent.png"/><span>Vent</span></a></li>
 <?
 }
 }
@@ -62,7 +62,7 @@ while($data = mysql_fetch_assoc($req))
 if(isset($_SESSION['auth']) && $_SESSION['niveau'] == 'admin')
 {
 ?>
-<li class="menu-configuration"><A HREF="./index.php?pages=administration"><img src="./img/icon_config.png"><span>Administration</span></a></li>
+<li class="menu-configuration"><A HREF="./index.php?page=administration"><img src="./img/icon_config.png"><span>Administration</span></a></li>
 
 <?
 }
