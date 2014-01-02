d9401cf264a7e7708073771ab1a56a92d4b67d6a
Cmoi20 <boquetj@yahoo.com>
2014-01-02 23:49:33 +0100

    bug import sql

--- www/sql/zibase.sql
+++ www/sql/zibase.sql
@@ -231,34 +231,6 @@ CREATE TABLE IF NOT EXISTS `auto-logon` (
 -- --------------------------------------------------------
 
 --
--- Contenu de la table `modules_accueil`
---
-
-INSERT INTO `modules_accueil` (`id`, `url`, `libelle`, `type`) VALUES
-(1, 'batterie', 'Batterie', ''),
-(2, 'conso_elec_tableau_global', 'Tableau de consomation electrique global', ''),
-(3, 'owl_global', 'Graphique quotidien OWL', ''),
-(4, 'temperature_graph_jour', 'Graphique journalier de temperature', 'temperature'),
-(5, 'vent_graph_rosedesvents', 'Rose des vents', 'vent'),
-(6, 'conso_elec_graph_annee', 'Graphique consomation electrique annuelle', 'conso'),
-(7, 'conso_elec_tableau_periph', 'Tableau consomation electrique', ''),
-(8, 'owl_phases_graph', 'Consomation electrique par phase OWL', ''),
-(9, 'temperature_graph_mois', 'Graphique temperature mensuelle', 'temperature'),
-(10, 'vent_tableau_global', 'Tableau vent', ''),
-(11, 'conso_elec_graph_journee', 'Graphique consomation electrique journaliere', 'conso'),
-(12, 'owl_batterie', 'Batterie OWL', 'owl'),
-(13, 'owl_tableau_global', 'Tableau OWL global', ''),
-(14, 'temperature_tableau_global', 'Tableau temperature global', ''),
-(15, 'conso_elec_graph_mois', 'Graphique consomation electrique mensuelle', 'conso'),
-(16, 'owl_global_month', 'Graphique consomation electrique mensuelle OWL', ''),
-(17, 'temperature_graph_annee', 'Graphique temperature annuelle', 'temperature'),
-(18, 'vent_graph_jour', 'Graphique vent journalier', 'vent'),
-(19, 'actioneur', 'Actioneur', 'actioneur'),
-(20, 'scenario', 'Scenario', 'scenario');
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `variables`
 --
 
@@ -314,6 +286,34 @@ CREATE TABLE IF NOT EXISTS `modules_accueil` (
   `type` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
+
+-- --------------------------------------------------------
+
+--
+-- Contenu de la table `modules_accueil`
+--
+
+INSERT INTO `modules_accueil` (`id`, `url`, `libelle`, `type`) VALUES
+(1, 'batterie', 'Batterie', ''),
+(2, 'conso_elec_tableau_global', 'Tableau de consomation electrique global', ''),
+(3, 'owl_global', 'Graphique quotidien OWL', ''),
+(4, 'temperature_graph_jour', 'Graphique journalier de temperature', 'temperature'),
+(5, 'vent_graph_rosedesvents', 'Rose des vents', 'vent'),
+(6, 'conso_elec_graph_annee', 'Graphique consomation electrique annuelle', 'conso'),
+(7, 'conso_elec_tableau_periph', 'Tableau consomation electrique', ''),
+(8, 'owl_phases_graph', 'Consomation electrique par phase OWL', ''),
+(9, 'temperature_graph_mois', 'Graphique temperature mensuelle', 'temperature'),
+(10, 'vent_tableau_global', 'Tableau vent', ''),
+(11, 'conso_elec_graph_journee', 'Graphique consomation electrique journaliere', 'conso'),
+(12, 'owl_batterie', 'Batterie OWL', 'owl'),
+(13, 'owl_tableau_global', 'Tableau OWL global', ''),
+(14, 'temperature_tableau_global', 'Tableau temperature global', ''),
+(15, 'conso_elec_graph_mois', 'Graphique consomation electrique mensuelle', 'conso'),
+(16, 'owl_global_month', 'Graphique consomation electrique mensuelle OWL', ''),
+(17, 'temperature_graph_annee', 'Graphique temperature annuelle', 'temperature'),
+(18, 'vent_graph_jour', 'Graphique vent journalier', 'vent'),
+(19, 'actioneur', 'Actioneur', 'actioneur'),
+(20, 'scenario', 'Scenario', 'scenario');
 
 -- --------------------------------------------------------
 
