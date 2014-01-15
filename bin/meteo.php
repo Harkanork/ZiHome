ef4f828b410d4657e6254e50e2b349959f598b9a
Cmoi20 <boquetj@yahoo.com>
2014-01-15 16:09:25 +0100

    correction de bug

--- bin/meteo.php
+++ bin/meteo.php
@@ -11,8 +11,10 @@ $zibase = new ZiBase($ipzibase);
 //Url a parser
 $weather = simplexml_load_file("http://wxdata.weather.com/wxdata/weather/local/".$meteo_ville."?cc=*&unit=m"); 
 
-// Temperature exterieure et humidité$zibase->sendVirtualProbeValues($meteo_sonde_temperature,$weather->cc->tmp*10,$weather->cc->hmid,0); 
-//123456788 : identifiant radio de la sonde sans OS devant, tmp*10 : il faut multiplier la tempéture par 10 car la zibase attend l.information au dixiè de degré// Vent 
+// Temperature exterieure et humidite
+$zibase->sendVirtualProbeValues($meteo_sonde_temperature,$weather->cc->tmp*10,$weather->cc->hmid,0); 
+//123456788 : identifiant radio de la sonde sans OS devant, tmp*10 : il faut multiplier la temperature par 10 car la zibase attend l.information au dixieme de degre
+// Vent 
 $zibase->sendVirtualProbeValues($meteo_sonde_vent,$weather->cc->wind->s*0.51,$weather->cc->wind->d,0);
-//Coefficient de conversion = 0.28 (la vitesse est exprimésur weather.com en m/s et sur zibase en km/h)
+//Coefficient de conversion = 0.28 (la vitesse est exprime sur weather.com en m/s et sur zibase en km/h)
 ?>
