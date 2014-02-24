2140f6c7c4aacae4275fa6cea8e817323073d05f
Harkanork <harkanork@gmail.com>
2014-02-24 22:51:36 +0100

    Compatibilite internet explorer Affichage des dates au format francais dans la page "affecter une température"

--- www/lib/date_francais.php
+++ www/lib/date_francais.php
@@ -1,6 +1,23 @@
 <?
 function date_francais($date){
-preg_match ('`^(\d{4})-(\d{2})-(\d{2})(.*)$`', $date, $out);
-return $out[3].'-'.$out[2].'-'.$out[1].'  '.$out[4];
+  if (preg_match ('`^(\d{4})-(\d{2})-(\d{2})(.*)$`', $date, $out))
+  {
+    return $out[3].'-'.$out[2].'-'.$out[1].'  '.$out[4];
+  }
+  else
+  {
+    return $date;
+  }
+}
+
+function date_ISO($date){
+  if (preg_match ('`^(\d{2})-(\d{2})-(\d{4})(.*)$`', $date, $out))
+  {
+    return $out[3].'-'.$out[2].'-'.$out[1].'  '.$out[4];
+  }
+  else
+  {
+    return $date;
+  }
 }
 ?>
--- www/pages/admin/affecter_sonde.php
+++ www/pages/admin/affecter_sonde.php
@@ -1,6 +1,7 @@
 <?
 if(isset($_SESSION['auth']) && $_SESSION['niveau'] == 'admin')
 {
+  include("./lib/date_francais.php");
 ?>
 <div id="action-actionneur">
   <center>
@@ -38,7 +39,8 @@ if(isset($_SESSION['auth']) && $_SESSION['niveau'] == 'admin')
 <?
   if(isset($_POST['id'])){
     include("./pages/connexion.php");
-    $query = "UPDATE peripheriques SET id_plan = '".$_POST['sonde']."', `top` = '".$_POST['top']."', `left` = '".$_POST['left']."', Icone = '".$_POST['icone']."', Texte = '".$_POST['texte']."', gerer_batterie = '".$_POST['gerer_batterie']."', libelle = '".$_POST['libelle']."', date_chgt_batterie = '".$_POST['date_chgt_batterie']."', show_value2 = '".$_POST['show_value2']."' WHERE nom = '".$_POST['id']."'";
+    $date_chgt_batterie = date_ISO($_POST['date_chgt_batterie']);
+    $query = "UPDATE peripheriques SET id_plan = '".$_POST['sonde']."', `top` = '".$_POST['top']."', `left` = '".$_POST['left']."', Icone = '".$_POST['icone']."', Texte = '".$_POST['texte']."', gerer_batterie = '".$_POST['gerer_batterie']."', libelle = '".$_POST['libelle']."', date_chgt_batterie = '".$date_chgt_batterie."', show_value2 = '".$_POST['show_value2']."' WHERE nom = '".$_POST['id']."'";
     mysql_query($query, $link);
   }
   include("./pages/connexion.php");
@@ -65,16 +67,27 @@ if(isset($_SESSION['auth']) && $_SESSION['niveau'] == 'admin')
     ?>
     </select>
     </td>
-    <td class="droite"><center><INPUT TYPE="number" NAME="left" VALUE="<? echo $data['left']; ?>" style="width:60px;"></center></td>
-    <td class="bas"><center><INPUT TYPE="number" NAME="top" VALUE="<? echo $data['top']; ?>" style="width:60px;"></center></td>
-    <td class="icone"><center><INPUT type="checkbox" name="icone" value="1"<? if($data['icone'] == "1"){ echo " checked"; } ?>></center></td>
-    <td class="icone"><center><INPUT type="checkbox" name="texte" value="1"<? if($data['texte'] == "1"){ echo " checked"; } ?>></center></td>    
-    <td class="icone"><center><INPUT type="checkbox" name="show_value2" value="1"<? if($data['show_value2'] == "1"){ echo " checked"; } ?>></center></td>
-    <td><center><INPUT type="checkbox" name="gerer_batterie" value="1"<? if($data['gerer_batterie'] == "1"){ echo " checked"; } ?>></td>
-    <td><INPUT type="date" name="date_chgt_batterie" size="10" value="<? echo $data['date_chgt_batterie']; ?>"></center></td>
-    <td><center><INPUT type="texte" name="libelle" value="<? echo $data['libelle']; ?>"></center></td>
-    <INPUT TYPE="HIDDEN" NAME="id" VALUE="<? echo $data['nom']; ?>">
-    <td class="input"><center><INPUT TYPE="SUBMIT" NAME="Valider" VALUE="Valider"></center></td>
+    <td class="droite"><center><INPUT TYPE="number" NAME="left" VALUE="<? echo $data['left']; ?>" style="width:60px;"/></center></td>
+    <td class="bas"><center><INPUT TYPE="number" NAME="top" VALUE="<? echo $data['top']; ?>" style="width:60px;"/></center></td>
+    <td class="icone"><center><INPUT type="checkbox" name="icone" value="1"<? if($data['icone'] == "1"){ echo " checked"; } ?>/></center></td>
+    <td class="icone"><center><INPUT type="checkbox" name="texte" value="1"<? if($data['texte'] == "1"){ echo " checked"; } ?>/></center></td>    
+    <td class="icone"><center><INPUT type="checkbox" name="show_value2" value="1"<? if($data['show_value2'] == "1"){ echo " checked"; } ?>/></center></td>
+    <td><center><INPUT type="checkbox" name="gerer_batterie" value="1"<? if($data['gerer_batterie'] == "1"){ echo " checked"; } ?>/></center></td>
+    <td> <INPUT id="date_chgt_batterie_<? echo $data['nom']; ?>" type="date" name="date_chgt_batterie" size="10"/>
+      <script>
+        if (!Modernizr.inputtypes['date']) 
+        {
+          document.getElementById('date_chgt_batterie_<? echo $data["nom"]; ?>').value = "<? echo date_francais($data['date_chgt_batterie']); ?>";
+        }
+        else
+        {
+          document.getElementById('date_chgt_batterie_<? echo $data["nom"]; ?>').value = "<? echo $data['date_chgt_batterie']; ?>";
+        }
+      </script>
+    </td>
+    <td><center><INPUT type="texte" name="libelle" value="<? echo $data['libelle']; ?>"/></center></td>
+    <INPUT TYPE="HIDDEN" NAME="id" VALUE="<? echo $data['nom']; ?>"/>
+    <td class="input"><center><INPUT TYPE="SUBMIT" NAME="Valider" VALUE="Valider"/></center></td>
     </FORM>
     </tr>
 <?
