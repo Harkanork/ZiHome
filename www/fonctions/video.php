b4a49f9fd576ca8e02e1ef8724b271aa328a87cd
Cmoi20 <boquetj@yahoo.com>
2014-03-18 15:17:12 +0100

    gestion des caméras

--- www/fonctions/video.php
+++ www/fonctions/video.php
@@ -1,11 +1,9 @@
 <div class="video">
-<video>
 <?
 $remote = explode(".", $_SERVER["REMOTE_ADDR"]);
 $local = explode(".", $_SERVER["SERVER_ADDR"]);
 if($remote[0] == $local[0] && $remote[1] == $local[1] && $remote[2] == $local[2]) { $address = $periph['adresse']; } else { $address = $periph['adresse_internet']; }
 ?>
-<source src="<? echo $address; ?>" width="<? echo $width; ?>" height="<? echo $height; ?>" controls autobuffer autoplay>
-</video>
+<img src="<? echo $address; ?>" width="<? echo $width; ?>" height="<? echo $height; ?>">
 <div class="panel"></div>
 </div>
