90efd08f15d938568dfa8251ab6724ebc916df83
Cmoi20 <boquetj@yahoo.com>
2014-03-19 14:06:01 +0100

    rafraichissement automatiquement des pages

--- www/pages/head.php
+++ www/pages/head.php
@@ -1,4 +1,5 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<META HTTP-EQUIV=Refresh CONTENT="600">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <link rel="apple-touch-icon-precomposed" href="img/icon-iphone.jpg" />
 <meta name="apple-mobile-web-app-capable" content="yes">
