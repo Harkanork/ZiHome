dae5e70d96eb4c34744068a25a1bd39950993663
Cmoi20 <boquetj@yahoo.com>
2014-03-26 16:46:44 +0100

    preparation temps activite actionneur

--- bin/actioneurs.php
+++ bin/actioneurs.php
@@ -25,6 +25,27 @@ while($i < $actionnb) {
   else if($actionlist[$i]['t'] == 'receiverXDom') {
     updateSensor($actionlist[$i], $info, $link, 'actioneur');
   }
+  if($actionlist[$i]['t'] == 'receiverXDom' || $actionlist[$i]['t'] == 'transmitter') {
+    $query = "SELECT * FROM peripheriques WHERE nom = '".$actionlist[$i]['n']."'";
+    $req = mysql_query($query, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+    while($data = mysql_fetch_assoc($req)) {
+      if($data['protocol'] == 6) {
+        $protocol = true;
+      } else {
+        $protocol = false;
+      }
+        $value = $zibase->getState($data['id'], $protocol);
+      }
+    $query = "CREATE TABLE IF NOT EXISTS `periph_".$actionlist[$i]['n']."` (`date` datetime NOT NULL, `actif` TINYINT(1) NOT NULL, PRIMARY KEY (`date`)) ENGINE=InnoDB DEFAULT CHARSET=latin1";
+    mysql_query($query, $link);
+    $query = "SELECT * FROM `periph_".$actionlist[$i]['n']."` ORDER BY `date` DESC LIMIT 1";
+    $req = mysql_query($query, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+    $data = mysql_fetch_assoc($req);
+      if(!($data['actif'] == $value)){
+        $query0 = "INSERT INTO `periph_".$actionlist[$i]['n']."` (date, actif) VALUES (now(), '".($value)."')";
+        mysql_query($query0, $link);
+      }
+  }
   $i++;
 }
 
