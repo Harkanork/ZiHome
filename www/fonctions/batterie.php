716b4d4a907c8adcfaaf1d6340be5f8a6bd4db58
Cmoi20 <boquetj@yahoo.com>
2014-04-23 11:56:37 +0200

    mise en forme batteries

--- www/fonctions/batterie.php
+++ www/fonctions/batterie.php
@@ -1,19 +1,20 @@
 <?php
 include("./lib/date_francais.php");
 include("./pages/connexion.php");
-
-echo "<CENTER><TABLE border=0 ><TR class='title' style='text-align: center'><TD>Sonde</TD><TD style='width:100px'>Dernier<br>changement</TD><TD style='width:140px'>Batterie<br>faible</TD></TR>";
+$i=0;
+echo "<CENTER><TABLE ><TR class='title' style='text-align: center'><TD><b>Sonde</b></TD><TD style='width:100px'><b>Dernier<br>changement</b></TD><TD style='width:140px'><b>Batterie<br>faible</b></TD></TR>";
 
 $query = "SELECT * FROM owl_detail ORDER BY `date` DESC LIMIT 1";
 $req = mysql_query($query, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
 while ($periph = mysql_fetch_assoc($req))
 {
   if($periph['battery'] == "100%") {
-    $batterie = $periph['battery'];
+    $batterie = "<img src='./img/batterie_ok.png' height='20px'/>";
   } else {
-    $batterie = "<FONT COLOR='red'>".$periph['battery']."</FONT>";
+    $batterie = "<img src='./img/batterie_ko.png' height='20px'/>";
   }
-  echo "<TR><TD>OWL CM180</TD><TD ALIGN=CENTER>".$batterie."</TD></TR>";
+  echo "<TR bgcolor='".( ($i % 2 == 1) ? '#dddddd' : '#eeeeee' )."'><TD>".$batterie."<span style='vertical-align:3px'>OWL CM180</span></TD><TD></TD><TD></TD></TR>";
+  $i++;
 }
 
 $query = "SELECT * FROM peripheriques WHERE gerer_batterie = '1'";
@@ -31,7 +32,8 @@ while ($periph = mysql_fetch_assoc($req))
   } else {
     $nom = $periph['libelle'];
   }
-  echo "<TR class=\"contenu\">";
+  echo "<TR class=\"contenu\" bgcolor='".( ($i % 2 == 1) ? '#dddddd' : '#eeeeee' )."'>";
+  $i++;
   echo "<TD>".$batterie."<span style='vertical-align:3px'>".$nom."</span></TD>";
   if ($periph['date_chgt_batterie'] != "0000-00-00")
   {
