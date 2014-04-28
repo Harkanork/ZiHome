c964bc161713c747b64b0903288998b2c361879f
Cmoi20 <boquetj@yahoo.com>
2014-04-28 14:27:38 +0200

    refonte des binaires de remonte des donnees

--- cron/zibase.bat
+++ cron/zibase.bat
@@ -1,10 +1,7 @@
 @echo off
-c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\actioneurs.php
-c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\conso_electrique.php
+c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\peripheriques.php
+c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\peripheriques_value.php
 c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\scenario.php
-c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\vent.php
-c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\temperature.php
-c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\message_zibase_auth.php
-c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\precipitation.php
 c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\iphone.php
-
+c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\meteo.php
+c:\wamp\bin\php\php5.4.16\php.exe -f c:\wamp\bin\zibase\pollution.php
