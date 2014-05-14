affafda131cbdbab9e87a3fbb62a0bd533cd43a6
Cmoi20 <boquetj@yahoo.com>
2014-05-14 18:28:16 +0200

    debug page d'accueil

--- www/pages/accueil.php
+++ www/pages/accueil.php
@@ -9,9 +9,8 @@ $req3 = mysql_query($query3, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_
 $data = mysql_fetch_array($req3);
 if(empty($data)){
 $query3 = "SELECT * FROM page_accueil WHERE user = 'default'";
-$req3 = mysql_query($query3, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
-$data = mysql_fetch_array($req3);
 }
+$req3 = mysql_query($query3, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
 while ($data3 = mysql_fetch_assoc($req3))
 {
 $width = $data3['width'];
