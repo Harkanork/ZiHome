0baf99c77ffea96d3647831e57e0187d16e18c72
Harkanork <harkanork@gmail.com>
2015-02-07 22:25:18 +0100

    conso_eau Correction du calcul de consommation Ajout du calcul de la conso annuelle moyenne

--- www/fonctions/conso_eau_tableau_periph.php
+++ www/fonctions/conso_eau_tableau_periph.php
@@ -1,7 +1,17 @@
 <?php
+
+function ajoutLigneConsoEau($couleur, $desc, $conso, $coutEau)
+{
+  if ($conso > 0)
+  {
+    echo "<TR bgcolor='" . $couleur . "' class=tab-ligne><TD>" . $desc . "</TD><TD style='text-align: right'>".number_format($conso,0, ',', ' ')." l&nbsp;</TD><TD style='text-align: right'>".number_format($conso / 1000, 2, ',', ' ')." m&sup3;&nbsp;</TD><TD style='text-align: right'>".number_format(($conso * $coutEau) / 1000, 2, ',', ' ')." &euro;&nbsp;</TD></TR>";
+  }
+}
+
+
 echo "<p class=titre-texte>Statistiques pour ".$nom." :</p>";
 echo "<p><CENTER><TABLE width=500px>";
-echo "<TR class=tab-titre><TD><b>P&eacute;riode</b></TD><TD width=120px><b>&nbsp;Consommation&nbsp;</b></TD><TD width=150px><b>&nbsp;Co&ucirc;t&nbsp;</b></TD></TR>";
+echo "<TR class=tab-titre><TD width=140px><b>P&eacute;riode</b></TD><TD width=120px><b>&nbsp;Conso. litres&nbsp;</b></TD><TD width=120px><b>&nbsp;Conso. m&sup3;&nbsp;</b></TD><TD width=150px><b>&nbsp;Co&ucirc;t&nbsp;</b></TD></TR>";
 
 // Calcul la consommation journaliere pour hier et aujourd'hui
 $query0 = "SELECT SUM(conso) as somme_conso, date FROM `eau_".$periph['nom']."` WHERE date > DATE_SUB(NOW(), INTERVAL 3 DAY) GROUP BY DATE_FORMAT(`date`, '%Y%m%d') ORDER BY DATE_FORMAT(`date`, '%Y%m%d') DESC LIMIT 2";
@@ -14,8 +24,33 @@ while ($value0 = mysql_fetch_assoc($req0))
   } else {
     $jour = "Hier";
   }
-  echo "<TR bgcolor='#eeeeee' class=tab-ligne><TD>" . $jour . "</TD><TD style='text-align: right'>".number_format($value0['somme_conso'],0, ',', ' ')." l&nbsp;</TD><TD style='text-align: right'>".number_format($value0['somme_conso'] * $coutEau,2,',',' ')." &euro;&nbsp;</TD></TR>";
+  ajoutLigneConsoEau('#eeeeee', $jour, $value0['somme_conso'], $coutEau);
   $i++;
+}
+
+// Calcul la consommation de la derniere annee
+$consoAnneeCourante = 0;
+$query0 = "SELECT SUM(conso) as somme_conso, date FROM `eau_".$periph['nom']."` WHERE  YEAR(date) = YEAR(curdate())";
+$req0 = mysql_query($query0, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+while($value0 = mysql_fetch_assoc($req0))
+{
+  $consoAnneeCourante = $value0['somme_conso'];
+}
+
+// Calcul de la valeur moyenne sur l'annee
+if ($consoAnneeCourante > 0)
+{
+  $query1 = "SELECT min(date) as min_date, max(date) as max_date FROM `eau_".$periph['nom']."` WHERE  YEAR(date) = YEAR(curdate())";
+  $req1 = mysql_query($query1, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+  while($value1 = mysql_fetch_assoc($req1))
+  {
+    $dateDebut = strptime($value1['min_date'], '%Y-%m-%e %k:%M:%S')[tm_yday];
+    $dateFin = strptime($value1['max_date'], '%Y-%m-%e %k:%M:%S')[tm_yday];
+    if ($dateDebut != $dateFin)
+    {
+      ajoutLigneConsoEau('#eeeeee', 'Moyenne annuelle', $consoAnneeCourante / ($dateFin - $dateDebut), $coutEau);
+    }
+  }
 }
 
 // Calcul la consommation de la semaine courante
@@ -23,7 +58,7 @@ $query0 = "SELECT SUM(conso) as somme_conso, date FROM `eau_".$periph['nom']."` 
 $req0 = mysql_query($query0, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
 while($value0 = mysql_fetch_assoc($req0))
 {
-  echo "<TR bgcolor='#dddddd' class=tab-ligne><TD>Semaine courante</TD><TD style='text-align: right'>".number_format($value0['somme_conso'],0, ',', ' ')." l&nbsp;</TD><TD style='text-align: right'>". number_format($value0['somme_conso'] * $coutEau,2,',',' ')." &euro;&nbsp;</TD></TR>";
+  ajoutLigneConsoEau('#dddddd', 'Semaine courante', $value0['somme_conso'], $coutEau);
 }
 
 // Calcul la consommation de la derniere semaine
@@ -31,7 +66,7 @@ $query0 = "SELECT SUM(conso) as somme_conso, date FROM `eau_".$periph['nom']."` 
 $req0 = mysql_query($query0, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
 while($value0 = mysql_fetch_assoc($req0))
 {
-  echo "<TR bgcolor='#dddddd' class=tab-ligne><TD>Semaine pr&eacute;c&eacute;dente</TD><TD style='text-align: right'>".number_format($value0['somme_conso'],0, ',', ' ')." l&nbsp;</TD><TD style='text-align: right'>". number_format($value0['somme_conso'] * $coutEau,2,',',' ')." &euro;&nbsp;</TD></TR>";
+  ajoutLigneConsoEau('#dddddd', 'Semaine pr&eacute;c&eacute;dente', $value0['somme_conso'], $coutEau);
 }
 
 // Calcul la consommation du mois courant
@@ -39,7 +74,7 @@ $query0 = "SELECT SUM(conso) as somme_conso, date FROM `eau_".$periph['nom']."` 
 $req0 = mysql_query($query0, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
 while($value0 = mysql_fetch_assoc($req0))
 {
-  echo "<TR bgcolor='#dddddd' class=tab-ligne><TD>Mois courant</TD><TD style='text-align: right'>".number_format($value0['somme_conso'],0, ',', ' ')." l&nbsp;</TD><TD style='text-align: right'>". number_format($value0['somme_conso'] * $coutEau,2,',',' ')." &euro;&nbsp;</TD></TR>";
+  ajoutLigneConsoEau('#eeeeee', 'Mois courant', $value0['somme_conso'], $coutEau);
 }
 
 // Calcul la consommation du dernier mois
@@ -47,15 +82,18 @@ $query0 = "SELECT SUM(conso) as somme_conso, date FROM `eau_".$periph['nom']."` 
 $req0 = mysql_query($query0, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
 while($value0 = mysql_fetch_assoc($req0))
 {
-  echo "<TR bgcolor='#dddddd' class=tab-ligne><TD>Mois pr&eacute;c&eacute;dent</TD><TD style='text-align: right'>".number_format($value0['somme_conso'],0, ',', ' ')." l&nbsp;</TD><TD style='text-align: right'>". number_format($value0['somme_conso'] * $coutEau,2,',',' ')." &euro;&nbsp;</TD></TR>";
+  ajoutLigneConsoEau('#eeeeee', 'Mois pr&eacute;c&eacute;dent', $value0['somme_conso'], $coutEau);
 }
 
-// Calcul la consommation de la derniere annee
-$query0 = "SELECT SUM(conso) as somme_conso, date FROM `eau_".$periph['nom']."` WHERE  YEAR(date) = YEAR(curdate())";
+// Affichage de la consommation de la derniere annee
+ajoutLigneConsoEau('#dddddd', 'Ann&eacute;e courante', $consoAnneeCourante, $coutEau);
+
+// Calcul la consommation de l'annee précédente
+$query0 = "SELECT SUM(conso) as somme_conso, date FROM `eau_".$periph['nom']."` WHERE  YEAR(date) = YEAR(curdate()) - 1";
 $req0 = mysql_query($query0, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
 while($value0 = mysql_fetch_assoc($req0))
 {
-  echo "<TR bgcolor='#dddddd' class=tab-ligne><TD>Ann&eacute;e</TD><TD style='text-align: right'>".number_format($value0['somme_conso'],0, ',', ' ')." l&nbsp;</TD><TD style='text-align: right'>". number_format($value0['somme_conso'] * $coutEau,2,',',' ')." &euro;&nbsp;</TD></TR>";
+  ajoutLigneConsoEau('#dddddd', 'Ann&eacute;e pr&eacute;c&eacute;dente', $value0['somme_conso'], $coutEau);
 }
 
 echo "</TABLE></CENTER></p>";
