ef4d827dff62dd68ec4b1c3c20c214065896a07f
Harkanork <harkanork@gmail.com>
2015-02-14 14:38:47 +0100

    Connexion automatique On ne peut plus rajouter d'appareil sans adresse mac Affichage d'un message quand la librairie ne réussi pas à trouver l'adresse mac

--- www/lib/mac_address.php
+++ www/lib/mac_address.php
@@ -1,17 +1,27 @@
 <?php
   $ipAddress=$_SERVER['REMOTE_ADDR'];
   $macAddr=false;
+  
+  // linux ?
   exec('/usr/sbin/arp -n ' . $ipAddress, $arp, $arp_ret);
   if ($arp_ret != 0)
   {
+    // essaie encore ?
     exec('arp -n ' . $ipAddress, $arp, $arp_ret);
+    if ($arp_ret != 0)
+    {
+      // Windows ?
+      exec('arp -a ' . $ipAddress, $arp, $arp_ret);
+    }
   }
+  
+
   foreach($arp as $line)
   {
      $cols=preg_split('/\s+/', trim($line));
      if ($cols[0]==$ipAddress)
      {
-         $macAddr=$cols[2];
+         $macAddr=$cols[1];
      }
      else if ($cols[1] == '(' . $ipAddress . ')')
      {
