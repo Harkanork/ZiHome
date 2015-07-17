d050b8c810bf0628c341b415b174b81bfcd349ef
Harkanork <harkanork@gmail.com>
2015-07-18 00:17:37 +0200

    Migration vers GitHub

--- www/pages/admin/update.php
+++ www/pages/admin/update.php
@@ -5,9 +5,9 @@ if(isset($_SESSION['auth']))
 <div id="body-action">
   <br>
   La version <? echo $version_server; ?> de ZiHome est disponible.<br>
-  La liste des nouveaut&eacute;s est accessible <a href="https://code.google.com/p/interface-utilisateur-domotique-zibase/wiki/PageChangeLog#<? echo $version_server; ?>">ici</a>.<br><br>
+  La liste des nouveaut&eacute;s est accessible <a href="https://github.com/Harkanork/ZiHome/wiki/PageChangeLog#<? echo str_replace(".", "", $version_server); ?>">ici</a>.<br><br>
   
-  <form method="get" action="https://interface-utilisateur-domotique-zibase.googlecode.com/archive/V<? echo $version_server; ?>.zip">
+  <form method="get" action="https://github.com/Harkanork/ZiHome/archive/V<? echo $version_server; ?>.zip">
     <INPUT TYPE="SUBMIT" VALUE="T&eacute;l&eacute;charger">
   </form>
 </div>
