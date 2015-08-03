2b847c862561528ffd9477b15bf017a39be1cdaf
Harkanork <harkanork@gmail.com>
2015-08-03 21:15:48 +0200

    Gestion des actionneurs RGB

--- www/pages/actioneur.php
+++ www/pages/actioneur.php
@@ -2,22 +2,36 @@
 session_start();
 if(isset($_SESSION['auth']))
 {
-?>
-<SCRIPT LANGUAGE="JavaScript"/>
-function redirect() {
-        window.location="javascript:history.go(-1)"
-}
-setTimeout("redirect()",0); // delai en millisecondes
-</SCRIPT>
-<?php
-include("../config/conf_zibase.php");
-include("../lib/zibase.php");
-include("../config/variables.php");
-$zibase = new ZiBase($ipzibase);
-if(isset($_GET['dim'])) {
-$zibase->sendCommand($_GET['action'], $_GET['ordre'], $_GET['protocol'], $_GET['dim']);
-} else {
-$zibase->sendCommand($_GET['action'], $_GET['ordre'], $_GET['protocol'], "");
-}
+  ?>
+  <SCRIPT LANGUAGE="JavaScript"/>
+    function redirect() 
+    {
+      window.location="javascript:history.go(-1)"
+    }
+    setTimeout("redirect()",0); // delai en millisecondes
+  </SCRIPT>
+  <?php
+  include("../config/conf_zibase.php");
+  include("../lib/zibase.php");
+  include("../config/variables.php");
+  $zibase = new ZiBase($ipzibase);
+
+  if (isset($_GET['rgb'])) 
+  {
+    $rgb = hexdec($_GET['rgb']);
+    $red = 0xFF & ($rgb >> 0x10);
+    $green = 0xFF & ($rgb >> 0x08);
+    $blue = 0xFF & $rgb;
+    
+    $zibase->sendRGBW($_GET['action'], $red, $green, $blue, 255);
+  } 
+  else if (isset($_GET['dim'])) 
+  {
+    $zibase->sendCommand($_GET['action'], $_GET['ordre'], $_GET['protocol'], $_GET['dim']);
+  } 
+  else 
+  {
+    $zibase->sendCommand($_GET['action'], $_GET['ordre'], $_GET['protocol'], "");
+  }
 }
 ?>
