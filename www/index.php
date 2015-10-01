22d01197198624f1c630f23196fe0927dbde00b2
endlersman <endlersman@gmail.com>
2015-10-01 17:07:26 +0200

    Modification de la structure de la page pour le nouveau template de menu personnalisable au dessus des vues personnalisables

--- www/index.php
+++ www/index.php
@@ -1,56 +1,49 @@
 <?
-if (!(file_exists("./config/conf_zibase.php")))
-{
-  include("./config/configuration.php");
-}
-else
-{
-  session_start();
-  
-  include("./pages/connexion.php");
-  include("./config/variables.php");
-  include("./pages/head.php");
-  include("./pages/menu.php");
-  
-  if (isset($_GET['page']))
-  {
-    include("./pages/".$_GET['page'].".php");
-  }
-  else
-  {
-    if (isset($_GET['include']))
-    {
-      $query = "SELECT * FROM `insertion` WHERE `id` = '".$_GET['include']."'";
-      $req = mysql_query($query, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
-      $data = mysql_fetch_assoc($req);
-      include($data['url']);
-    }
-    else
-    {
-      if (isset($_SESSION['auth']))
-      {
-        $query = "SELECT * FROM users WHERE pseudo = '".$_SESSION['auth']."'";
-        $req = mysql_query($query, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
-        while ($data = mysql_fetch_assoc($req))
-        {
-          $accueil = $data['accueil'];
-        }
-      }
-      if (!(isset($accueil)) || (isset($accueil) && $accueil == ""))
-      {
-        $query = "SELECT * FROM paramettres WHERE libelle = 'accueil'";
-        $req = mysql_query($query, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
-        while ($data = mysql_fetch_assoc($req))
-        {
-          $accueil = $data['value'];
-        }
-        if (!(isset($accueil)))
-        {
-          $accueil = 'plan';
-        }
-      }
-      include("./pages/".$accueil.".php");
-    }
-  }
+if(!(file_exists("./config/conf_zibase.php"))) {
+	include("./config/configuration.php");
+} else {
+	session_start();
+	include("./pages/connexion.php");
+	include("./config/variables.php");
+	include("./pages/head.php");
+	include("./pages/menu.php");
+	echo '<div id="contenu">
+	<script>
+		var sousbandeau = false;
+	</script>
+	';
+	if(isset($_GET['page'])){
+		include("./pages/".$_GET['page'].".php");
+	} else if(isset($_GET['interne'])){
+		$query = "SELECT * FROM `menu` WHERE `id` = '".$_GET['interne']."'";
+		$req = mysql_query($query, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+		$data = mysql_fetch_assoc($req);
+		include($data['url']);
+	} else if (isset($_GET['iframe'])) {
+		include("./pages/iframe.php");
+	} else if (isset($_GET['vue'])) {
+		include("./pages/vue.php");
+	} else {
+		if(isset($_SESSION['auth'])) {
+			$query = "SELECT * FROM users WHERE pseudo = '".$_SESSION['auth']."'";
+			$req = mysql_query($query, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+			while ($data = mysql_fetch_assoc($req)) {
+				$accueil = $data['accueil'];
+			}
+		}
+		if(!(isset($accueil)) || (isset($accueil) && $accueil == "")) {
+			$query = "SELECT * FROM paramettres WHERE libelle = 'accueil'";
+			$req = mysql_query($query, $link) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+			while ($data = mysql_fetch_assoc($req)) {
+				$accueil = $data['value'];
+			}
+			if(!(isset($accueil))) {
+				$accueil = 'plan';
+			}
+			echo '<script>sousbandeau = true ;</script>';
+		}
+		include("./pages/".$accueil.".php");
+	}
+	echo '</div>';
 }
 ?>
