0f6d2a6b06ec5ee4b56cfab6f3cba0502063c41b
endlersman <endlersman@gmail.com>
2015-10-01 17:17:45 +0200

    Quelques modifs à la marge, dernier fichier modifié, peut être pas terminé ...

--- www/pages/admin/gerer_modules.php
+++ www/pages/admin/gerer_modules.php
@@ -12,17 +12,14 @@ if(isset($_SESSION['auth']))
         Nom
         </TH>
         <TH>
-        Action
-        </TH>
-        <TH>
-        Ordre
+        Actif ?
         </TH>
         <TH>
         </TH>
       </tr>
 <?
       if(isset($_POST['id'])){
-        $query = "UPDATE modules SET actif = '".$_POST['actif']."', ordre = '".$_POST['ordre']."' WHERE id = '".$_POST['id']."'";
+        $query = "UPDATE modules SET actif = '".$_POST['actif']."' WHERE id = '".$_POST['id']."'";
         mysql_query($query, $link);
         echo "<meta http-equiv=\"refresh\" content=\"0; url=./index.php?page=administration&detail=gerer_modules\">";
       }
@@ -33,7 +30,7 @@ if(isset($_SESSION['auth']))
 ?>
       <tr class="contenu">
         <FORM method="post" action="./index.php?page=administration&detail=gerer_modules">
-          <td width="190px" align="center">
+          <td width="300px" align="center">
             <? echo $data['libelle']; ?>
           </td>
           <td>
@@ -43,10 +40,7 @@ if(isset($_SESSION['auth']))
             </select>
           </td>
           <td>
-            <input type="number" name="ordre" value="<? echo $data['ordre']; ?>"></input>
-          </td>
             <INPUT TYPE="HIDDEN" NAME="id" VALUE="<? echo $data['id']; ?>">
-          <td>
             <INPUT TYPE="SUBMIT" NAME="Valider" VALUE="Valider">
           </td>
         </FORM>
